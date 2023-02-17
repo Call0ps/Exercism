431bb98cb2beffb93f0539fb5d93cfa799e6db62
Carl Emil Lidbom <lidbom.calle@outlook.com>
2023-02-17 23:40:01 +0100

    .

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,10 +1,20 @@
 #include "triangle.h"
+#include <stdexcept>
 
 namespace triangle {
 
-flavor kind(double x, double y, double z) {
-  if (x == y == z)
-    return flavor::equilateral;
-    
-}
+    flavor kind(double x, double y, double z) {
+        if (x <= 0)
+            throw std::domain_error("X is 0 or less");
+        else if (y <= 0)
+            throw std::domain_error("Y is 0 or less");
+        else if (z <= 0)
+            throw std::domain_error("Z is 0 or less");
+        else if((x+y)<z)
+            throw std::domain_error("Not a valid triangle.");
+        if (x == y == z)
+            return flavor::equilateral;
+
+        return flavor::scalene;
+    }
 } // namespace triangle
